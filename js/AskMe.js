1170ad7806be3c76bb5f0dbfc757487a96e18355
Melanie Levy-Hara <melanie.levy.hara@gmail.com>
2014-12-10 22:41:47 -0500

    Adding controllers
    I believe using controllers will fix my problem with javascript. Will
    look into this.

--- js/AskMe.js
+++ js/AskMe.js
@@ -1,8 +1,10 @@
         $(document).ready(function(){
+            alert('Hello');
             // set up hover panels
             // although this can be done without JavaScript, we've attached these events
             // because it causes the hover to be triggered when the element is tapped on a touch device
-            $('.hover').hover(function(){
+            $('.ng-scope').click(function(){
+                alert('Hello');
                 $(this).addClass('flip');
             },function(){
                 $(this).removeClass('flip');
